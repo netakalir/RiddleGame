f42cc4ced66d20dbca998bb1efe80515c709c17a
Netanel kalir <netanelkalir@gmail.com>
2025-07-03 12:23:41 +0300

    fix comment from CR

--- riddleGame/apps.js
+++ riddleGame/apps.js
@@ -1,39 +1,5 @@
-import rl from "readline-sync";
-import { Player } from "../classes/Player.js";
-//ייבוא של החידות
-import { riddle1 } from "../riddles/r1.js";
-import { riddle2 } from "../riddles/r2.js";
-//שמירה של החידות לתוך מערך
-const riddles = [riddle1, riddle2];
+import { startRiddleGame } from "../managment/managerRiddle.js";
 
 
-function sayHello(){
-    console.log("-----welcome to the riddle game-----")
-    const name = rl.question("-------what is your name?-----")
-    console.log(`--------hello ${name}--------`)
-    return name;
-}
-
-function initPlayer(){
-   const player = new Player(sayHello());
-   return player;
-}
-
-function calcTimes(cb,player){
-    const start = Date.now();
-    cb()
-    const end = Date.now();
-    const seconds = Math.floor((end - start)/1000);
-    player.recordTime(seconds)
-}
-
-function startRiddleGame() {
-    const player = initPlayer()
-    
-    for(let i = 0; i < riddles.length; i++){
-        calcTimes(() => riddles[i].ask(),player)
-    }
-    player.showStats()
-}
 
 startRiddleGame()
