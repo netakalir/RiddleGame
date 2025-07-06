0b1f8c993941de588b6dc9eb7fb398753a37633b
Netanel kalir <netanelkalir@gmail.com>
2025-07-06 17:49:30 +0300

    more utils

---
+++ utils/helperFunctions.js
@@ -0,0 +1,22 @@
+
+export function calcTimes(cb) {
+    const start = Date.now();
+    cb()
+    const end = Date.now();
+    return Math.floor((end - start) / 1000);
+    // player.recordTime(seconds)
+}
+// export function calcTimes(cb, player) {
+//     const start = Date.now();
+//     cb()
+//     const end = Date.now();
+//     const seconds = Math.floor((end - start) / 1000);
+//     player.recordTime(seconds)
+// }
+
+function sayHello() {
+    console.log("-----welcome to the riddle game-----")
+    const name = rl.question("-------what is your name?-----")
+    console.log(`--------hello ${name}--------`)
+    return name;
+}
