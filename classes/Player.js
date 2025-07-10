ca33eec1bef6b05ae856f3b4aa84461f1bfd5e5a
Netanel kalir <netanelkalir@gmail.com>
2025-07-10 12:06:43 +0300

    player class update

--- classes/Player.js
+++ classes/Player.js
@@ -1,46 +1,13 @@
-import fs from "fs/promises"
-import { readPlayers } from "../services/players.services.js";
 class Player {
     constructor(player) {
         this.id = player.id;
         this.name = player.name;
-        this.times = player.times || []; 
-        this.LowestTimes = player.LowestTimes;
+        this.times = player.times || [];
+
     }
 
-    async RC(second){
-        this.times.push(second)
-        const oldData = await readPlayers();
-        
-    }
-    
     async recordTime(second) {
-        console.log(this);
         this.times.push(second);
-        let data;
-        data = await readPlayers()
-        // try {
-        // } catch (error) {
-        //     console.log(error);
-        // }
-        console.log(data);
-        const newData = JSON.parse(data)
-        const index= newData.findIndex((user)=> {
-            console.log(user.id, this.id);
-            return user.id === this.id;
-        });
-        if(index === -1){
-           return; 
-        }
-        newData[index].times.push(second);
-        fs.writeFile("./DB/players.txt",JSON.stringify(newData),"utf-8",(err)=>{
-            if (err){
-                console.log("time update was faild." , err);
-            }
-            else{
-                console.log("time update was successful");
-            }
-        })
     }
 
     getLowestTimes() {
