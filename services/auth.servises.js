b31e92738c4c531da6e41d8674fcc1203a8950f4
Netanel kalir <netanelkalir@gmail.com>
2025-07-24 14:22:03 +0300

    register added

--- services/auth.servises.js
+++ services/auth.servises.js
@@ -1,5 +1,16 @@
+import rl from "readline-sync"
+
+
 export async function register() {
-    const response = await fetch("http://localhost:3005/auth/register")
+    const name = rl.question("insert your name:> ")
+    const password = rl.question("insert password:> ")
+    const response = await fetch("http://localhost:3005/auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({name,password})
+    })
     return await response.json()
 }
 
