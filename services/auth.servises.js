c2b33b0da80fc4ddd7178f8e3967d19a7bed0d13
Netanel kalir <netanelkalir@gmail.com>
2025-07-27 11:52:31 +0300

    token complit

--- services/auth.servises.js
+++ services/auth.servises.js
@@ -3,7 +3,7 @@ import rl from "readline-sync"
 
 export async function register() {
     const name = rl.question("insert your name:> ")
-    const password = rl.question("insert password:> ")
+    const password = rl.question("insert new password:> ")
     const response = await fetch("http://localhost:3005/auth/register", {
         method: "POST",
         headers: {
@@ -14,7 +14,34 @@ export async function register() {
     return await response.json()
 }
 
+
 export async function login() {
-    const response = await fetch("http://localhost:3005/auth/login")
-    return await response.json()
+    const name = rl.question("insert your name:> ")
+    const password = rl.question("insert your password:> ")
+    const response = await fetch("http://localhost:3005/auth/login",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name,password})
+    })
+
+    if(!response.ok){
+        const error = await response.json();
+        console.log("Login failed:", error.error);
+        return null;
+    }
+    const res = await response.json()
+    setToken(res.token)
+    return res
+}
+
+let token = null;//global veriabal to store at token
+
+export function setToken(value) {
+    token = value;
+}
+
+export function getToken() {
+    return token;
 }
