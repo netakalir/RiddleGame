1e968d70a8a5bad3620ea50d78af985259d8198a
Netanel kalir <netanelkalir@gmail.com>
2025-07-27 11:52:54 +0300

    token complit

--- services/riddles.services.js
+++ services/riddles.services.js
@@ -1,13 +1,25 @@
 import rl from "readline-sync";
-
+import { getToken } from "./auth.servises.js";
 
 export async function getAllRiddle() {//get all riddles by requesting from the server
-    const response = await fetch("http://localhost:3005/riddles/getAllRiddle")
+    const response = await fetch("http://localhost:3005/riddles/getAllRiddle", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${getToken()}` // שימוש בטוקן שנשמר קודם
+        }
+    })
     return await response.json()
 }
 
 export async function showAllRiddle() {//show all riddles by requesting from the server
-    const res = await fetch("http://localhost:3005/riddles/getAllRiddle");
+    const res = await fetch("http://localhost:3005/riddles/getAllRiddle", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${getToken()}` // שימוש בטוקן שנשמר קודם
+        }
+    });
     const response = await res.json();
     console.log("\n--- All Riddles ---");
     response.riddles.forEach((riddle, i) => {
@@ -29,7 +41,8 @@ export async function createRiddle() {//create riddle by requesting from the ser
     const response = await fetch("http://localhost:3005/riddles/createRiddle", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${getToken()}` // שימוש בטוקן שנשמר קודם
         },
         body: JSON.stringify(riddle)
     });
@@ -47,7 +60,10 @@ export async function updateRiddle() {//update riddle by requesting from the ser
 
     const response = await fetch(`http://localhost:3005/riddles/updateRiddle/${id}`, {
         method: "PUT",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${getToken()}` // שימוש בטוקן שנשמר קודם
+        },
         body: JSON.stringify(riddle)
     })
     const data = await response.json()
@@ -57,8 +73,12 @@ export async function updateRiddle() {//update riddle by requesting from the ser
 export async function deleteRiddle() {//delete riddle by requesting from the server
     const id = rl.question("Enter riddle ID to delete:> ");
 
-    const response = await fetch(`/${id}`, {
-        method: "DELETE"
+    const response = await fetch(`http://localhost:3005/riddles/deleteRiddle/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${getToken()}` // שימוש בטוקן שנשמר קודם
+        },
     });
 
     const data = await response.json();
